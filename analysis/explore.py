be1d87fd8815036e67b27930254a305c8d56a89b
pabloem <pabloem@ucla.edu>
2015-07-08 11:50:37 +0200

    Moving variable

--- analysis/explore.py
+++ analysis/explore.py
@@ -6,6 +6,25 @@ import os.path
 
 G = nx.read_graphml('hanja_unip_2hanwds_03062015.graphml',unicode)
 
+
+similar_pairs =[
+    [u'同',u'一'], #Dong - same, Il - one
+    [u'兩',u'二'], #Yang - both, I - two
+    [u'共',u'同'], #Gong - together, Dong - same
+    [u'側',u'面'], #Cheuk - side, Myeon - surface, plane, side
+    [u'法',u'則'], #Beob - Law, Chik - Rule
+    [u'職',u'線'], #Jik - straight, Seon - line
+    [u'父',u'母'], #Bu - father, Mo - mother
+    [u'業',u'職'], #Eob - business trade, Jik - duty, profession
+    [u'人',u'者'],  #In - person, Ja - That, who, he
+    [u'回',u'環'], # Hoe - return, turn around, Hwan - ring, bracelet
+    [u'見',u'視'], # Kyeon - Observe, percieve, Shi - Inspect, observe, see
+    [u'聞',u'聽'] # Moon - Hear, make known, Cheong - Hear, listen
+    # Shik - formula, Gyu - regulation
+    # Hyeong - shape, Mo - standard, model
+    # Chong - General, all, whole
+]
+
 # The 2-hanja graph projection is not connected, thus we must get the largest
 # connected component of the graph.
 gen = nxa.connected_components(G)
@@ -36,24 +55,6 @@ if not os.path.isfile('distcount.json'):
     # We divide by 2, because we double-counted every pair
     for num in distance_count:
         distance_count[num] = distance_count[num]/2
-
-    similar_pairs =[
-        [u'同',u'一'], #Dong - same, Il - one
-        [u'兩',u'二'], #Yang - both, I - two
-        [u'共',u'同'], #Gong - together, Dong - same
-        [u'側',u'面'], #Cheuk - side, Myeon - surface, plane, side
-        [u'法',u'則'], #Beob - Law, Chik - Rule
-        [u'職',u'線'], #Jik - straight, Seon - line
-        [u'父',u'母'], #Bu - father, Mo - mother
-        [u'業',u'職'], #Eob - business trade, Jik - duty, profession
-        [u'人',u'者'],  #In - person, Ja - That, who, he
-        [u'回',u'環'], # Hoe - return, turn around, Hwan - ring, bracelet
-        [u'見',u'視'], # Kyeon - Observe, percieve, Shi - Inspect, observe, see
-        [u'聞',u'聽'] # Moon - Hear, make known, Cheong - Hear, listen
-        # Shik - formula, Gyu - regulation
-        # Hyeong - shape, Mo - standard, model
-        # Chong - General, all, whole
-    ]
 
     for lst in similar_pairs:
         h1 = lst[0]

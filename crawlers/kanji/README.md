6f603c28708514853939081cb1222f58e7818e53
pabloem <polecito.em@gmail.com>
2015-09-22 11:02:41 +0900

    Kanji scraper - and scrape results

---
+++ crawlers/kanji/README.md
@@ -0,0 +1,5 @@
+These are the utilities to scrape the Kanji information in
+'http://www.manythings.org/kanji/d/'. They all serve different purposes.
+
+* `scrape.py` - This is the main scraper. It gets the data and outputs a JSON file with
+  words, and Kanjis. This JSON file can be used to generate the graphml file.
--- crawlers/kanji/scrape.py
+++ crawlers/kanji/scrape.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python
 from bs4 import BeautifulSoup
 import requests
+import json
+import io
+import sys
 
 class MTKanjiScraper:
     def __init__(self,config):
@@ -9,15 +13,17 @@ class MTKanjiScraper:
         self._words = []
         self._wordSet = set()
         self._kanjiSet = set()
-        self._missingKanji = set()
+        self._missingKanji = []
         self._linkDict = {}
         self._requests = 0
+        self._limit = int(config['limit']) if 'limit' in config else 0
         pass
 
     def _get_the_soup(this,url):
         print("GETSOUP: Scraping page: "+url)
         r = requests.get(url)
-        sp = BeautifulSoup(r.text)
+        r.encoding = 'shiftjis'
+        sp = BeautifulSoup(r.text,from_encoding="Shift_JIS")
         this._requests += 1
         return sp
 
@@ -31,8 +37,8 @@ class MTKanjiScraper:
         uls = sp.select('ul')
         li0 = uls[0].select('li')
         english = li0[0].text
-        onyomi = li0[1].text
-        kunyomi = li0[2].text
+        onyomi = li0[1].text if len(li0) > 1 else ""
+        kunyomi = li0[2].text if len(li0) > 2 else ""
 
         res['chinese'] = kanji.strip()
         res['english'] = english.strip()
@@ -40,9 +46,12 @@ class MTKanjiScraper:
         res['kunyomi'] = kunyomi.strip()
         self._kanjis.append(res)
 
-        li1 = uls[1].select('li')
-        li3 = uls[3].select('li')
-        wordlis = li1 + li3
+        wordlis = []
+        for elm in uls[1:]:
+            if len(elm.select('b')) == 0:
+                continue
+            wordlis += elm.select('li')
+
         for li in wordlis:
             wordRes = {}
             w_chinese = li.select('b')[0].text
@@ -58,7 +67,7 @@ class MTKanjiScraper:
                 continue
             for character in w_chinese:
                 if character not in self._kanjiSet:
-                    self._missingKanji.add(character)
+                    self._missingKanji.append(character)
             self._words.append(wordRes)
         
 
@@ -81,7 +90,28 @@ class MTKanjiScraper:
         for kanji in self._linkDict:
             count += 1
             self.scrape_kanji(kanji)
-            if count == 10: return
+            if count == self._limit: break
+            if count % 500 == 0:
+                print("Scraped "+str(count)+" kanjis.")
+
+    def store_json(self,filename):
+        with io.open(filename,'w',encoding='utf-8') as f:
+            res = {'words':self._words,
+                   'kanji':self._kanjis,
+                   'missing_kanji':self._missingKanji}
+            f.write(unicode(json.dumps(res,ensure_ascii=False,
+                                       sort_keys=True, indent=2)))
+        f.close()
 
 config = {'base_url':'http://www.manythings.org/kanji/d/',
-          'index_files':['index.html']}#,'index2.html','index3.html','index4.html','index5.html']}
+          'index_files':['index.html','index2.html','index3.html','index4.html','index5.html']}
+
+if len(sys.argv) < 2:
+    print("Usage: ./scrape.py output_file [limit]")
+    sys.exit(0)
+
+if len(sys.argv) > 2: config['limit'] = sys.argv[2]
+
+s = MTKanjiScraper(config)
+s.go()
+s.store_json(sys.argv[1])
